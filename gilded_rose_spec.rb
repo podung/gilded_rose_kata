0b20235db9983f27fef0fcae6651884a6d29d05a
Joe DePung <joe.depung@gmail.com>
2014-10-29 21:06:45 -0500

    setup specific items for test

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -3,17 +3,28 @@ require "rspec"
 require "rspec/its"
 
 describe GildedRose do
+  let(:dexterity) { Item.new("+5 Dexterity Vest", 10, 20) }
+  let(:aged_brie) { Item.new("Aged Brie", 2, 0) }
+  let(:elixir)    { Item.new("Elixir of the Mongoose", 5, 7) }
+  let(:sulfuras)  { Item.new("Sulfuras, Hand of Ragnaros", 0, 80) }
+  let(:backstage) { Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20) }
+  let(:conjured)  { Item.new("Conjured Mana Cake", 3, 6) }
 
   describe "update_quality" do
     let(:described) { described_class.new }
 
     before do
-      described.update_quality
+      described.items = items
     end
 
-    subject { described.items.find { |i| i.name == fine_cheese } }
-
     context "when standard items" do
+      let(:items) { [ dexterity, elixir ] }
+
+      before do
+        described.update_quality
+      end
+
+      subject { described.items.find { |i| i.name == fine_cheese } }
 
       context "when +5 Dexterity Vest" do
         let (:fine_cheese) { "+5 Dexterity Vest" }
