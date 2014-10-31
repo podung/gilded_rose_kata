fe5b05540b206bafb9907d8791e3d15497b40e5f
Joe DePung <joe.depung@gmail.com>
2014-10-31 06:28:26 -0500

    replace strings with constants

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,33 +1,39 @@
 require './item.rb'
 
 class GildedRose
-  attr_accessor :items
+  DEXTERITY = "+5 Dexterity Vest"
+  AGED_BRIE = "Aged Brie"
+  ELIXIR    = "Elixir of the Mongoose"
+  SULFURAS  = "Sulfuras, Hand of Ragnaros"
+  BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+  CONJURED  = "Conjured Mana Cake"
 
+  attr_accessor :items
   @items = []
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    @items << Item.new(DEXTERITY, 10, 20)
+    @items << Item.new(AGED_BRIE, 2, 0)
+    @items << Item.new(ELIXIR, 5, 7)
+    @items << Item.new(SULFURAS, 0, 80)
+    @items << Item.new(BACKSTAGE, 15, 20)
+    @items << Item.new(CONJURED, 3, 6)
   end
 
   def update_quality
 
     for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+      if (@items[i].name != AGED_BRIE && @items[i].name != BACKSTAGE)
         if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+          if (@items[i].name != SULFURAS)
             @items[i].quality = @items[i].quality - 1
           end
         end
       else
         if (@items[i].quality < 50)
           @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+          if (@items[i].name == BACKSTAGE)
             if (@items[i].sell_in < 11)
               if (@items[i].quality < 50)
                 @items[i].quality = @items[i].quality + 1
@@ -41,14 +47,14 @@ class GildedRose
           end
         end
       end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+      if (@items[i].name != SULFURAS)
         @items[i].sell_in = @items[i].sell_in - 1;
       end
       if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (@items[i].name != AGED_BRIE)
+          if (@items[i].name != BACKSTAGE)
             if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+              if (@items[i].name != SULFURAS)
                 @items[i].quality = @items[i].quality - 1
               end
             end
