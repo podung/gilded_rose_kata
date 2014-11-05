d20065dc5acffbfa82e194a87262151aa6b5e542
Joe DePung <joe.depung@gmail.com>
2014-11-04 22:27:42 -0600

    just make it a sub class

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -48,4 +48,3 @@ class GildedRose
     }
   end
 end
-
--- item_wrapper.rb
+++ item_wrapper.rb
@@ -1,47 +1,30 @@
 require './item.rb'
 
-class ItemWrapper
-  attr_reader :name, :quality, :sell_in
-
+class ItemWrapper < Item
   MAX_QUALITY = 50
   MIN_QUALITY = 0
 
-  def initialize(name, sell_in, quality)
-    @item = Item.new(name, sell_in, quality)
-  end
-
-  def name
-    @item.name
-  end
-
-  def quality
-    @item.quality
-  end
-
-  def sell_in
-    @item.sell_in
-  end
-
   def increase_quality
-    @item.quality += 1 if @item.quality < MAX_QUALITY
+    @quality += 1 if @quality < MAX_QUALITY
   end
 
   def reduce_quality
-    @item.quality -= 1 if @item.quality > MIN_QUALITY
+    @quality -= 1 if @quality > MIN_QUALITY
   end
 
   def zero_out_quality
-    @item.quality = 0
+    @quality = 0
   end
+
   def reduce_sell_in
-    @item.sell_in -= 1
+    @sell_in -= 1
   end
 
   def increase_sell_in
-    @item.sell_in -= 1
+    @sell_in -= 1
   end
 
   def expired?
-    @item.sell_in <= 0
+    @sell_in <= 0
   end
 end
