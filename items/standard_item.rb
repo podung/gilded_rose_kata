6c52b182d104921f8a2415978ef1a462f962220e
Joe DePung <joe.depung@gmail.com>
2014-11-05 23:38:43 -0600

    remove unneeded initialize method in standard_item

--- items/standard_item.rb
+++ items/standard_item.rb
@@ -1,10 +1,6 @@
 require_relative 'item_wrapper.rb'
 
 class StandardItem < ItemWrapper
-  def initialize(name, sell_in, quality)
-    super(name, sell_in, quality)
-  end
-
   def update_quality
     reduce_quality
     reduce_quality if expired?
