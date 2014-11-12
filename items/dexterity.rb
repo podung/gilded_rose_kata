956f9adf78225d0bd83a4e3c9b437c421ff3c832
Joe DePung <joe.depung@gmail.com>
2014-11-11 20:11:16 -0600

    remove accidental double definition of elixer and conjured in dexterity file

--- items/dexterity.rb
+++ items/dexterity.rb
@@ -5,15 +5,3 @@ class Dexterity < StandardItem
     super("+5 Dexterity Vest", sell_in, quality)
   end
 end
-
-class Elixer < StandardItem
-  def initialize(sell_in, quality)
-    super("Elixir of the Mongoose", sell_in, quality)
-  end
-end
-
-class Conjured < StandardItem
-  def initialize(sell_in, quality)
-    super("Conjured Mana Cake", sell_in, quality)
-  end
-end
